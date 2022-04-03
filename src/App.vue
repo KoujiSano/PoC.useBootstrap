8dadc1c3acf7f1f947e660f8c9e6c31de2684213
KoujiSano <sano@simplise.jp>
2022-04-03 18:00:39 +0900

    4 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,33 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-   <BvBadge background-color="secondary">New</BvBadge>
-     <BvAccordion parent>
-    <BvAccordionItem active>
-      <BvAccordionHeader level="2">
-        <BvAccordionButton>Accordion Item #1</BvAccordionButton>
-      </BvAccordionHeader>
-      <BvAccordionCollapse>
-        <BvAccordionBody>
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-          <code>.accordion-body</code>, though the transition does limit overflow.
-        </BvAccordionBody>
-      </BvAccordionCollapse>
-    </BvAccordionItem>
-    <BvAccordionItem>
-      <BvAccordionHeader level="2">
-        <BvAccordionButton>Accordion Item #2</BvAccordionButton>
-      </BvAccordionHeader>
-      <BvAccordionCollapse>
-        <BvAccordionBody>
-          <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-          <code>.accordion-body</code>, though the transition does limit overflow.
-        </BvAccordionBody>
-      </BvAccordionCollapse>
-    </BvAccordionItem>
-    <BvAccordionItem>
-      <BvAccordionHeader level="2">
-        <BvAccordionButton>Accordion Item #3</BvAccordionButton>
-      </BvAccordionHeader>
-      <BvAccordionCollapse>
-        <BvAccordionBody>
-          <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-          <code>.accordion-body</code>, though the transition does limit overflow.
-        </BvAccordionBody>
-      </BvAccordionCollapse>
-    </BvAccordionItem>
-  </BvAccordion>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <BvNavbar expand="lg" theme="light" background-color="light">
+    <BvContainer type="fluid">
+      <BvNavbarBrand>Navbar</BvNavbarBrand>
+      <BvNavbarToggler></BvNavbarToggler>
+      <BvNavbarCollapse>
+        <BvNavbarNavList margin="e-auto b-2 b-lg-0">
+          <BvNavItem>
+            <BvNavLink active>Home</BvNavLink>
+          </BvNavItem>
+          <BvNavItem>
+            <BvNavLink>Link</BvNavLink>
+          </BvNavItem>
+          <BvNavItem>
+            <BvNavLink disabled>Disabled</BvNavLink>
+          </BvNavItem>
+        </BvNavbarNavList>
+        <BvForm flex role="search">
+          <BvFormInput margin="e-2" type="search" placeholder="Search"></BvFormInput>
+          <BvButton button="outline-success" type="submit">Search</BvButton>
+        </BvForm>
+      </BvNavbarCollapse>
+    </BvContainer>
+  </BvNavbar>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
